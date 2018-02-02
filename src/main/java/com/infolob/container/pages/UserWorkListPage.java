3812d5f259d21767fffa2650889c47747535e2a5
loko <binario200@gmail.com>
2018-02-01 23:03:22 -0600

    fixing chrome version, adding validation for platform at UserWorkList page adding chromedriver to test locally adding log report instrucctions

--- src/main/java/com/infolob/container/pages/UserWorkListPage.java
+++ src/main/java/com/infolob/container/pages/UserWorkListPage.java
@@ -6,24 +6,39 @@ import org.openqa.selenium.WebElement;
 
 public class UserWorkListPage {
 
-	private WebDriver driver;
-	private WebElement workListRow;
+ private WebDriver driver;
+ private WebElement workListRow;
 
 
-	public UserWorkListPage(WebDriver driver)
-	{
-		this.driver = driver;
-	}
+ public UserWorkListPage(WebDriver driver)
+ {
+  this.driver = driver;
+ }
+ private static String OS = null;
+ public static String getOsName()
+ {
+  if(OS == null) { OS = System.getProperty("os.name"); }
+  return OS;
+ }
+ public static boolean isWindows()
+ {
+  return getOsName().startsWith("Windows");
+ }
 
-	public void goToPurchaseRequisitionPage(int requisitionNumber)
-	{
-		workListRow =  driver.findElement(By.xpath("//*[@id=\"NtfWorklist:Content\"]/tbody/tr[1]/td[3]"));
-		System.out.println(workListRow.getText());
-		workListRow.click();
-	}
+ public void goToPurchaseRequisitionPage(int requisitionNumber)
+ {
+  if(isWindows())
+  {
+   workListRow =  driver.findElement(By.xpath("//*[@id=\"NtfWorklist:Content\"]/tbody/tr[1]/td[3]"));
+   System.out.println(workListRow.getText());
+   workListRow.click();
+  }else
+  {
+   workListRow =  driver.findElement(By.xpath("//*[@id=\"N41:NtfSubject:0\"]"));
+   System.out.println(workListRow.getText());
+   workListRow.click();
+  }
+
+ }
 
 }
-
-
-
-
